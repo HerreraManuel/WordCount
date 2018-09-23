0192f6d423f5e537b171a5b14e7d1787b2b28b99
Manuel Herrera <mherrera@csus.edu>
2018-09-23 01:23:53 -0700

    Compact wordCount, lineCount, characterCount method.

--- src/wc.java
+++ src/wc.java
@@ -24,51 +24,26 @@ public class wc {
             String[] lineHolder;
             BufferedReader reader= new BufferedReader (new FileReader(fileName));
             while((line = reader.readLine()) != null) {
-                lineHolder = line.split("\\s");
-                if (lineHolder[0].equals("") && lineHolder.length > 1)
-                wordCounter += lineHolder.length;
+                lineHolder = line.replaceAll("\\s+", " ").split(" ");
+                for (String singleWord : lineHolder) {
+                    characterCount += singleWord.length();
+                    if (!singleWord.isEmpty())
+                        wordCounter++;
+                }
                 lineCounter++;
             }
             System.out.println(lineCounter);
             System.out.println(wordCounter);
+            System.out.println(characterCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    // wc -l prints line count of the file
-    private static int lineCount(String fileName) {
-        int lineCounter = 0;
-        // insert directory name to the File object
-        File currentFile = new File(fileName);
-        try {
-            String line;
-            BufferedReader reader = new BufferedReader(new FileReader(currentFile));
-            while ((line = reader.readLine()) != null) lineCounter++;
-            return lineCounter;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        return 0;
-    }
-    private static int wordCount(String fileName){
-        int wordCounter = 0;
-        File currentFile = new File(fileName);
-        try {
-            String line;
-            BufferedReader reader = new BufferedReader(new FileReader(currentFile));
-            while ((line = reader.readLine()) != null) {
-                String[] holder = line.split("\\s");
-                    wordCounter += holder.length;
-            }
-            return wordCounter;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }        return 0;
-    }
+
 
     public static void main(String[] args) {
         // Checking to see if any file was inserted.
-        read("C:\\Users\\mherr\\Desktop\\Java HW\\WordCount\\src\\random.py");
+        read("C:\\Users\\mherr\\Desktop\\Java HW\\WordCount\\src\\random.txt");
         /*if (args.length > 0) {
             //System.out.println(lineCount(args[0]));
             System.out.println(wordCount("C:\\Users\\mherr\\Desktop\\Java HW\\WordCount\\src\\random.py"));
