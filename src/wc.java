aed2438ad23b57261ee91e4bf7fbbf086ed904bd
Manuel Herrera <mherrera@csus.edu>
2018-10-02 21:16:20 -0700

    Sprint 2; getExtension, picocli

--- src/wc.java
+++ src/wc.java
@@ -1,5 +1,6 @@
 /* Manuel Herrera
- * Fall 2018 */
+ * Fall 2018
+ * Sprint 2*/
 
 /*
 Counting and file reading methods obtained from:
@@ -8,144 +9,83 @@ https://stackoverflow.com/questions/4094119/counting-number-of-words-in-a-file
 https://stackoverflow.com/questions/16802147/java-i-want-to-read-a-file-name-from-command-line-then-use-a-bufferedreader-to
  */
 
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+import java.io.*;
+import java.util.List;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+@picocli.CommandLine.Command(name = "Metrics", footer = "\nCSC131: Individual Project - Sprint 2. Design document available.", description =
+        "If no option is declared, prints lines, words, character, comment lines, source line counts for each" +
+                "FILE, and a total line if more than one FILE is specified. Otherwise, prints specified request for FILE(s).\n" +
+                "\"Source lines of code\" is determined on the [author & journal].\n", sortOptions = false)
+
+public class Metrics {
+    public long numLines;
+    public long numWords;
+    public long numChars;
+    public String fileExt;
+
+    public Metrics() {}
+
+    @Parameters(paramLabel = "Stat Requested", description = "Contents to display") List<File> files;
+    @Option(names = {"-l", "--lines"}, description = "Print the number of lines") boolean lineStat;
+    @Option(names = {"-w", "--words"}, description = "Print the number of words") boolean wordStat;
+    @Option(names = {"-c", "--characters"}, description = "Print the number of characters") boolean charStat;
+    @Option(names = {"-s", "--sourceLines"}, description = "Print the number of source lines") boolean srcLnStat;
+    @Option(names = {"-C", "--commentlines"}, description = "Print the number of comment lines") boolean cmtStat;
+    @Option(names = {"-h", "--help"}, usageHelp = true,  description = "Display this help and exit") boolean help;
 
 
-public class wc {
-    private int totalLines;
-    private int totalWords;
-    private int totalChars;
-
-    private wc() {
-        totalLines = 0;
-        totalWords = 0;
-        totalChars = 0;
-    }
-
-    // invalid stat request.
-    static void err (char errInput) throws IOException {
-        throw new IOException("wc: invalid option --" + errInput);
-    }
-
-    // No file, prints instructions
-    private static void usage() {
-        System.out.println("wc will print instructions for how to use wc \n" +
-                "wc -l <filename> will print the line count of a file\n" +
-                "wc -c <filename> will print the character count\n" +
-                "wc -w <filename> will print the word count\n" +
-                "wc <filename> will print all the above");
-    }
-
-    // Read the file(s).
-    public void read(String[] args, int lines, int words, int characters) throws IOException {
-        String[] filePaths = new String[args.length - 1];
-        for (int i = 0; i < args.length - 1; i++)
-            filePaths[i] = args[i + 1];
+    public void run(List<File> inFiles) throws Exception {
         try {
-            for (String currentFile : filePaths) {
-                BufferedReader lineReader = new BufferedReader(new FileReader(currentFile));
-                BufferedReader wordReader = new BufferedReader(new FileReader(currentFile));
-                BufferedReader charReader = new BufferedReader(new FileReader(currentFile));
-                File onFile = new File(currentFile);
-                if (lines == 0 && words == 0 && characters == 0){
-                    System.out.println(lineCount(lineReader) + " " + wordCount(wordReader)
-                            + " " + charCount(charReader) + " " + onFile.getName());
-                    lines = 1;
-                    words = 1;
-                    characters = 1;
-                }
-                else {
-                    if (lines == 1) System.out.print(lineCount(lineReader) + " ");
-                    if (words == 1) System.out.print(wordCount(wordReader) + " ");
-                    if (characters == 1) System.out.print(charCount(charReader) + " ");
-                    System.out.print(onFile.getName() + "\n");
-                }
-             }
-            printTotals(lines, words, characters);
-        } catch (IOException e) {
-                 e.printStackTrace();
+            for (File temp : inFiles) {
+                lineCount(temp);
+                wordAndCharCount(temp);
+                getExtension(temp);
             }
-}
-
-    public int lineCount (BufferedReader singleFile) throws IOException{
-        String line;
-        int numOfLines = 0;
-        while((line = singleFile.readLine()) != null ){
-            numOfLines++;
+            System.out.println("Line Count = " + numLines);
+            System.out.println("Word Count = " + numWords);
+            System.out.println("Char Count = " + numChars);
+            System.out.println("File Ext = " + fileExt);
+        }catch (IOException e) {
+            e.printStackTrace();
         }
-        totalLines += numOfLines;
-        return numOfLines;
     }
 
-    public int wordCount (BufferedReader singleFile) throws IOException{
-        int numOfWords = 0;
-        String line;
-        String[] lineHolder;
-        while ((line = singleFile.readLine()) != null) {
-            lineHolder = line.replaceAll("\\s+", " ").split(" ");
-            for (String singleWord : lineHolder)
-                if (!singleWord.isEmpty()) numOfWords++;
-        }
-        totalWords += numOfWords;
-        return numOfWords;
+    public void lineCount(File currFile) throws Exception{
+        BufferedReader reader = new BufferedReader(new FileReader(currFile));
+        numLines = reader.lines().count();
+        //while(reader.readLine() != null) numLines++;
     }
 
-    public int charCount (BufferedReader singleFile) throws IOException {
-        int numOfChar = 0;
-        String line;
-        String[] lineHolder;
-        while((line = singleFile.readLine()) != null) {
-            lineHolder = line.replaceAll("\\s+", " ").split(" ");
-            for (String singleWord: lineHolder) {
-                numOfChar += singleWord.length();
+    public void wordAndCharCount(File currFile) throws Exception{
+        BufferedReader wordReader = new BufferedReader(new FileReader(currFile));
+        String lineHolder;
+        while ((lineHolder = wordReader.readLine()) != null){
+            numChars += lineHolder.length();
+            if (!lineHolder.isEmpty()){
+                String[] lineOfWords = lineHolder.trim().split("\\s+");
+                numWords += lineOfWords.length;
             }
         }
-        totalChars += numOfChar;
-        return numOfChar;
     }
 
-    private void printTotals(int lines, int words, int characters) {
-        String finalLine = new String();
-        if (lines == 1) finalLine += totalLines + " ";
-        if (words == 1) finalLine += totalWords + " ";
-        if (characters == 1) finalLine += totalChars + " ";
-        System.out.println(finalLine + " total");
+    public void getExtension(File tempFile){
+        fileExt = tempFile.getName().substring(tempFile.getName().indexOf("."));
     }
 
-
-    public static void main(String[] args) throws IOException{
-        if (args.length < 1)
-            usage();
+    public static void main(String[] args){
+        if (args.length < 1) picocli.CommandLine.usage(new Metrics(), System.out);
         else {
-            int charPos = 1;
-            int lines = 0, words = 0, characters = 0;
-            wc filesToBeRead = new wc();
-            if (args[0].charAt(0) != '-') filesToBeRead.read(args, lines, words, characters);
-            else {
-                while (charPos < args[0].length()) {
-                    switch (args[0].charAt(charPos)) {
-                        case 'w':
-                            words++;
-                            break;
-                        case 'c':
-                            characters++;
-                            break;
-                        case 'l':
-                            lines++;
-                            break;
-                        default :
-                            err (args[0].charAt(1));
-                            System.exit(0);
-                    }
-                    charPos++;
-                }
-                // send triggers to retrieve correct stats.
-                filesToBeRead.read(args, lines, words, characters);
+            //Metrics m = new Metrics();
+            try {
+                Metrics m = picocli.CommandLine.populateCommand(new Metrics(), args);
+                if (m.help) { picocli.CommandLine.usage(new Metrics(), System.out);}
+                if (m.lineStat) { m.run(m.files);}
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+
         }
 
     }
