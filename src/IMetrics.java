246d2bc765a8bbdf266d14ae5129adf8410e3190
HerreraManuel <mherrera@csus.edu>
2018-10-25 00:53:51 -0700

    setPath() created. Fix output calculations.

--- src/IMetrics.java
+++ src/IMetrics.java
@@ -1,7 +1,7 @@
 
 interface IMetrics {
 
-    //boolean setPath(String path);     // sets the file path to process
+    boolean setPath(String path);     // sets the file path to process
                                         // returns true if current path is valid
     boolean isSource();                 // returns true if the file is a source file
 
@@ -13,18 +13,18 @@ interface IMetrics {
 
     // source code line counts
     //
-    /*
+
     int getSourceLineCount();
     int getCommentLineCount();
-    */
+
     // Halstead metrics
     //
-    /*
+
     int getHalsteadn1();            // number of distinct operands
     int getHalsteadn2();            // number of distinct operators
     int getHalsteadN1();            // number of operands
     int getHalsteadN2();            // number of operators
-
+//
     int getHalsteadVocabulary();
     int getHalsteadProgramLength();
     int getHalsteadCalculatedProgramLenght();
@@ -33,5 +33,5 @@ interface IMetrics {
     int getHalsteadEffort();
     int getHalsteadTime();
     int getHalsteadBugs();
-    */
+
 }
